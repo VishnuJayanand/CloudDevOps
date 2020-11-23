9bb92c015258e436577cd33286f9d35511c03145
Vishnu Jayanand <vishnu.jayanand@st.ovgu.de>
2020-11-23 17:04:28 +0000

    script

--- feature031/main.sh
+++ feature031/main.sh
@@ -1,1 +1,13 @@
+#! /bin/bash
+
+sudo apt-get update
+sudo apt-get install nginx
+sudo nginx
+sudo ufw enable
+sudo ufw allow 'Nginx full'
+sudo ufw allow 'Nginx HTTP'
+sudo ufw allow 'Nginx HTTPS'
+sudo nginx
+sudo apt install curl
+
 curl --silent --head http://localhost
