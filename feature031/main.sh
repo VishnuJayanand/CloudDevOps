75260b529aaacb208f2b1b0d0f60ef86b8a65b79
Vishnu Jayanand <vishnu.jayanand@st.ovgu.de>
2020-11-24 14:51:19 +0000

    removed sudo commands

--- feature031/main.sh
+++ feature031/main.sh
@@ -1,13 +1,7 @@
 #! /bin/bash
 
 sudo apt-get update
-sudo apt-get install nginx
-sudo nginx
-sudo ufw enable
-sudo ufw allow 'Nginx full'
-sudo ufw allow 'Nginx HTTP'
-sudo ufw allow 'Nginx HTTPS'
-sudo nginx
-sudo apt install curl
-
+apt-get install nginx
+nginx
+systemctl start nginx
 curl --silent --head http://localhost
