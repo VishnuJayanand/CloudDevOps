ac5e27c78a34b2d0cbcb20546d3713670b0e65d0
Vishnu Jayanand <vishnu.jayanand@st.ovgu.de>
2021-01-12 12:45:41 +0000

    test

--- feature049/main.py
+++ feature049/main.py
@@ -9,9 +9,13 @@ username = "admin"
 password = "admin"
 
 connection = fabric.Connection(host=host, port=22, user=username, connect_kwargs={'password': password})
-print("connection succeeded")
-connection.run("pwd")
+#print("connection succeeded")
+#connection.run("pwd")
 
 final_key = "ssh-rsa " + open('public_key.pub').read()
+connection.run('mkdir -p ~/.ssh/')
+connection.run('echo "%s" > ~/.ssh/authorized_keys' % final_key)
+connection.run('cat ~/.ssh/authorized_keys')
 
-#Create ssh folder and
+
+
