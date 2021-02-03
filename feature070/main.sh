164d88644b970763f0482588a9cde0d17f95f92b
Vishnu Jayanand <vishnu.jayanand@st.ovgu.de>
2021-02-03 18:31:08 +0000

    Update 'feature070/main.sh'

--- feature070/main.sh
+++ feature070/main.sh
@@ -2,4 +2,4 @@
 
 token=$(curl -X GET "http://rest.api.local:8000/token/vishnu.jayanand" -H  "Accept: application/json" | grep -oP '(?<="token": ")[^"]*')
 
-curl -d '{"key1":"hpdfjpdvnrsazsnzwojsyvqpsrgbseyt", "key2":"hbzkquxe", "key3": "qkfiaqfmzdafjbym"}' -H "Content-Type: application/json" -X POST http://rest.api.local:8000/data/$token
+curl -d '{"key1":"hpdfjpdvnrsazsnzwojsyvqpsrgbseyt", "key2":"hbzkquxe", "key3": "qkfiaqfmzdafjbym"}' -H "Content-Type: application/json" -H "Accept: application/json" -X POST http://rest.api.local:8000/data/$token
