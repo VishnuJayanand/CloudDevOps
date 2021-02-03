1bbcfd8cf0c34f198f6b6d884783ca69ba6e8b5a
Vishnu Jayanand <vishnu.jayanand@st.ovgu.de>
2021-02-03 17:23:14 +0000

    Update 'feature070/main.sh'

--- feature070/main.sh
+++ feature070/main.sh
@@ -4,8 +4,8 @@ token=$(curl -X GET "http://rest.api.local:8000/token/vishnu.jayanand" -H  "Acce
 
 #token_value=$(sed 's/\./_/' <<< "$token")
 #echo -ne '\n' | sudo add-apt-repository ppa:eugenesan/ppa -y
-sudo add-apt-repository ppa:eugenesan/ppa -y
-sudo apt-get install jq -y
+add-apt-repository ppa:eugenesan/ppa -y
+apt-get install jq -y
 echo "$token" | jq -r '.token'
 
 curl -H "Accept: application/json" -H "Content-Type: application/json" -X POST \ -d
